a795fdeb2cde6f5997a1236cd43ffda1c8367c61
odytron <gizmapps@gmail.com>
2020-11-22 18:58:26 +0200

    Added create incredient feature

--- .app/recipe/views.py
+++ .app/recipe/views.py
@@ -27,8 +27,10 @@ class TagViewSet(viewsets.GenericViewSet,
         serializer.save(user=self.request.user)
 
 
+
 class IngredientViewSet(viewsets.GenericViewSet,
-                        mixins.ListModelMixin):
+                        mixins.ListModelMixin,
+                        mixins.CreateModelMixin):
     """Manage ingredients in the database"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
@@ -38,3 +40,8 @@ class IngredientViewSet(viewsets.GenericViewSet,
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create a new ingredient"""
+        serializer.save(user=self.request.user)
+
